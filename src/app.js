d36a5795176d2417cc18ef4aa5e42ac65f3aeee4
Carlos <carlossantana@Natalie-Morriss-MacBook-Pro.local>
2020-07-07 12:25:33 -0400

    fixed cors issue

--- src/app.js
+++ src/app.js
@@ -5,9 +5,8 @@ import indexRouter from './routes/index';
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000'); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
