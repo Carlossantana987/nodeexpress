0621085076de29c886ed4acf451877c113851e71
Carlos <carlossantana@Natalie-Morriss-MacBook-Pro.local>
2020-07-07 13:02:30 -0400

    fixed cors issue

--- src/app.js
+++ src/app.js
@@ -5,15 +5,6 @@ import indexRouter from './routes/index';
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Max-Age", "1800");
-	res.setHeader("Access-Control-Allow-Headers", "content-type");
-	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-  next();
-});
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +13,15 @@ app.use('/v1', indexRouter);
 
 app.use((err, req, res, next) => {
   res.status(400).json({ error: err.stack });
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  next();
 });
 
 export default app;
