4cc67ce1dd6a7753daa6ef9777127d1e166fea4c
Carlos <carlossantana@Natalie-Morriss-MacBook-Pro.local>
2020-07-07 13:45:41 -0400

    adding cors package to handle CORS policy

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
 
+
+const cors = require('cors')
 const app = express();
+
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -13,15 +17,6 @@ app.use('/v1', indexRouter);
 
 app.use((err, req, res, next) => {
   res.status(400).json({ error: err.stack });
-});
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Max-Age", "1800");
-	res.setHeader("Access-Control-Allow-Headers", "content-type");
-	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-  next();
 });
 
 export default app;
